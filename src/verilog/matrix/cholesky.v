adf3239c791cecfd60f80734e6852324fa16d9b1
Hatim Kanchwala <hatim@hatimak.me>
2017-10-29 18:51:29 +0530

    Compute l_22 outside normal machines

--- src/verilog/matrix/cholesky.v
+++ src/verilog/matrix/cholesky.v
@@ -29,19 +29,19 @@ module cholesky #(parameter SIZE = 4) (
     localparam S_T_ROUTE  = 2'b01;
     localparam S_T_I2     = 2'b11;
 
-    wire            sqrt_ready_a11, en_trav, ready_li2;
-    wire [63:0]     sqrt_y_a11;
-    wire [SIZE-2:0] fpu_i1_ready;
+    wire                     sqrt_ready_a11, en_trav, ready_li2, fpu_mul_22_ready, fpu_add_22_ready, sqrt_22_ready;
+    wire [63:0]              fpu_mul_22_out, fpu_add_22_out;
+    wire [SIZE-2:0]          fpu_i1_ready;
     // Since SIZE is at least 2, keep an additional bit (MSB) which will remain unused.
     // For SIZE=2, compute array for l_i,2 (i > 2) is NOT triggered.
-    wire [SIZE-2:0] fpu_mul_i2_ready, fpu_add_i2_ready, fpu_div_i2_ready;
+    wire [SIZE-2:0]          fpu_mul_i2_ready, fpu_add_i2_ready, fpu_div_i2_ready;
     wire [((SIZE-1)*64)-1:0] fpu_mul_i2_out, fpu_add_i2_out; // Keep an additional vector (MSB).
-    reg [1:0]       state_trav;
-    reg             en_i2;
-    reg [2:0]       tmr_i2;
+    reg [1:0]                state_trav;
+    reg                      en_i2;
+    reg [2:0]                tmr_i2;
 
     sqrt sqrt_a11 (
-        .y      (sqrt_y_a11), // a_11
+        .y      (factor[63:0]), // l_11 = sqrt(a_11)
         .ready  (sqrt_ready_a11),
         .x      (matrix[63:0]), // a_11
         .enable (enable),
@@ -62,7 +62,7 @@ module cholesky #(parameter SIZE = 4) (
                 .rmode     (ROUND),
                 .fpu_op    (FPU_OP_DIV),
                 .opa       (matrix[IND_I1+63:IND_I1]), // a_i,1
-                .opb       (sqrt_y_a11), // sqrt(a_11)
+                .opb       (factor[63:0]), // l_11
                 .out       (factor[IND_I1+63:IND_I1]), // l_i,1
                 .ready     (fpu_i1_ready[i1-1]),
                 .underflow (),
@@ -74,7 +74,51 @@ module cholesky #(parameter SIZE = 4) (
         end
     endgenerate
 
-    assign en_trav = &fpu_i1_ready;
+    // Since SIZE is at least 2, l_22 needs to be computed in any case.
+    // l_22 = sqrt(a_22 - l_21 * l_21)
+    fpu fpu_mul_22 (
+            .clk       (clk),
+            .rst       (rst),
+            .enable    (fpu_i1_ready[0]), // Enable as soon as l_21 computation is ready (irrespective of other l_i,1).
+            .rmode     (ROUND),
+            .fpu_op    (FPU_OP_MUL),
+            .opa       (factor[(SIZE*64)+63:SIZE*64]), // l_21
+            .opb       (factor[(SIZE*64)+63:SIZE*64]), // l_21
+            .out       (fpu_mul_22_out), // l_21 * l_21
+            .ready     (fpu_mul_22_ready),
+            .underflow (),
+            .overflow  (),
+            .inexact   (),
+            .exception (),
+            .invalid   ()
+            );
+    fpu fpu_add_22 (
+            .clk       (clk),
+            .rst       (rst),
+            .enable    (fpu_mul_22_ready),
+            .rmode     (ROUND),
+            .fpu_op    (FPU_OP_ADD),
+            .opa       (matrix[((SIZE+1)*64)+63:(SIZE+1)*64]), // a_22
+            .opb       (fpu_mul_22_out | (1 << 63)), // -(l_21 * l_21)
+            .out       (fpu_add_22_out), // a_22 - l_21 * l_21
+            .ready     (fpu_add_22_ready),
+            .underflow (),
+            .overflow  (),
+            .inexact   (),
+            .exception (),
+            .invalid   ()
+            );
+    sqrt sqrt_22 (
+        .y      (factor[((SIZE+1)*64)+63:(SIZE+1)*64]), // l_22
+        .ready  (sqrt_22_ready),
+        .x      (fpu_add_22_out), // a_22 - l_21 * l_21
+        .enable (fpu_add_22_ready),
+        .clk    (clk),
+        .rst    (rst)
+        );
+
+    // Trigger Traversal machine once all l_i,1 and l_22 are computed.
+    assign en_trav = &fpu_i1_ready & sqrt_22_ready;
 
     // Traversal machine, triggered by en_trav.
     always @(posedge clk or negedge clk) begin
@@ -170,9 +214,8 @@ module cholesky #(parameter SIZE = 4) (
         end
     endgenerate
 
-    // Generate ready_li2 signal when all div_i2 are ready.
-
-    assign factor[63:0] = sqrt_y_a11; // l_11 = sqrt(a_11)
+    assign fpu_div_i2_ready[SIZE-2] = 1'b1; // Set fpu_*_i2_ready[SIZE-2] to high (SIZE-2 is unused MSB).
+    assign fpu_mul_i2_ready = &fpu_div_i2_ready; // Used in S_T_I2 which is only reached when SIZE > 2.
 
     // Cholesky factor is lower triangular, so l_ij = 0 for j > i (i = 1, 2, 3, ..., n-1).
     generate
